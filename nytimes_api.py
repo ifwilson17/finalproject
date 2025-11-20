7b1e26fbbd99e350b53bab64a10138e6a0ac9420
Isa <ifwilson@umich.edu>
2025-11-20 11:14:49 -0500

    draft of first function

--- nytimes_api.py
+++ nytimes_api.py
@@ -1,2 +1,25 @@
+API_KEY = ""
+
 def get_nyt_reviews(movie_titles):
-    pass
+    base_url = "https://api.nytimes.com/svc/movies/v2/reviews/search.json"
+    reviews_list = []
+
+    for title in movie_titles: 
+        params = {'query': title, 'apikey': API_KEY}
+
+        try: 
+            response = requests.get(base_url, params)
+            data = json.loads(response.text)
+        except: 
+            print("Request Failed")
+            continue
+
+        results = data.get("results")
+        if not results: 
+            continue 
+        
+        
+
+        reviews_list.append(reviews_dict)
+
+    return reviews_list
